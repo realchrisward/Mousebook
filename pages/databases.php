34ca4de8d3eb5cea5a8cf2e1d496776650269a55
Chris Ward <ward.chris.s@gmail.com>
2025-11-05 10:38:56 -0600

    debug

--- pages/databases.php
+++ pages/databases.php
@@ -91,7 +91,7 @@
 		</h1>
 					 	 
 		<form id="loginbox" action="" method="post">
-			<table class="table-center", id="server_sign_in">
+			<table class="table server_sign_in">
 				<tr>
 					<th>Server Connection Status</th>
 					<td>
@@ -100,7 +100,7 @@
 					</td>
 				</tr>
 			</table>
-			<table class="table-center", id="server_sign_in">
+			<table class="table server_sign_in">
 				<tr>
 					<th>USERNAME</th>
 					<td>
